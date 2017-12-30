c66de18ae99683bde9d701a9534f538546b2b17f
tutunak <dk@tutunak.com>
2017-12-30 15:38:18 +0300

    add new models

--- gifs/models.py
+++ gifs/models.py
@@ -1,39 +1,57 @@
 from django.db import models
 
-class Blog(models.Model):
-    name = models.TextField()
-    url = models.URLField(name="Blog url")
-    offset = models.IntegerField(name='Offset for next end search', blank=True, null=True)
-    full_parse = models.BooleanField(name="All old posts is parsed", default=False)
-    # When checked - Status True. When all blogs checked - set for all True
-    checked = models.BooleanField(name="Checked last time", default=False)
+# class Blog(models.Model):
+#     name = models.TextField()
+#     url = models.URLField(name="Blog url")
+#     offset = models.IntegerField(name='Offset for next end search', blank=True, null=True)
+#     full_parse = models.BooleanField(name="All old posts is parsed", default=False)
+#     # When checked - Status True. When all blogs checked - set for all True
+#     checked = models.BooleanField(name="Checked last time", default=False)
+#
+#     def __str__(self):
+#         return str(self.name)
+#
+# class Tag_source(models.Model):
+#     tag = models.TextField(name='Tag on tumblr')
+#
+#     def __str__(self):
+#         return str(self.tag)
+#
+# class Post(models.Model):
+#     id = models.BigIntegerField(name='Post id')
+#     type = models.TextField(name='Type of post')
+#     url = models.URLField(name='Link to post')
+#     blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+#     tag = models.ManyToManyField(Tag_source)
+#
+#     def __str__(self):
+#         return str(self.id)
+#
+# class Media(models.Model):
+#     url = models.URLField(unique=True)
+#     file = models.TextField(name='File on disk')
+#     type = models.TextField(name='Type of file')
+#     post = models.ForeignKey(Post)
+#
+#     def __str__(self):
+#         return str(self.url)
 
-    def __str__(self):
-        return str(self.name)
 
-class Tag_source(models.Model):
-    tag = models.TextField(name='Tag on tumblr')
+class Tag(models.Model):
+    tag = models.TextField()
 
-    def __str__(self):
-        return str(self.tag)
 
 class Post(models.Model):
-    id = models.BigIntegerField(name='Post id')
-    type = models.TextField(name='Type of post')
-    url = models.URLField(name='Link to post')
-    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
-    tag = models.ManyToManyField(Tag_source)
+    tumblr_post_id = models.BigIntegerField()
+    timestamp = models.IntegerField(null=False)
+    json = models.TextField()
 
-    def __str__(self):
-        return str(self.id)
 
-class Media(models.Model):
-    url = models.URLField(unique=True)
-    file = models.TextField(name='File on disk')
-    type = models.TextField(name='Type of file')
+class Tagged(models.Model):
     post = models.ForeignKey(Post)
-
-    def __str__(self):
-        return str(self.url)
+    tag = models.ForeignKey(Tag)
 
 
+class GifLink(models.Model):
+    link = models.URLField()
+    post = models.ForeignKey(Post)
