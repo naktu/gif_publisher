e5469d7f2f339b7879b0f965bb56697fb581a562
tutunak <dk@tutunak.com>
2018-02-10 19:24:23 +0300

    upload method for docs added

--- gifs/utils/vk.py
+++ gifs/utils/vk.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 
@@ -7,13 +8,20 @@ class Vk:
             'access_token': access_token,
             'v': v
         }
-        self.url = 'https://api.vk.com/method/'
+        self.main_url = 'https://api.vk.com/method/'
 
     def get(self, method, **kwargs):
-        self.url += method
+        url = self.main_url + method
         self.params.update(kwargs)
-        print(self.params)
-        return requests.get(self.url, params=self.params).json()
+        resp = requests.get(url, params=self.params)
+        if resp.ok:
+            return resp.json()
+        else:
+            return {'error': {
+                'status_code': resp.status_code,
+                'reason': resp.reason
+                         }
+                    }
 
 
 class Group:
@@ -32,7 +40,6 @@ class Group:
                                   filter='postponed',
                                   owner_id=self.id
                                   )
-
         self.response = self.result.get('response', None)
         if self.response:
             self.count = self.response.get('count', None)
@@ -51,3 +58,23 @@ class Group:
             if self.response:
                 self.last_postponed = self.response.get('items', None)
             return self.last_postponed
+
+    def upload_doc(self, file):
+        method = 'docs.getUploadServer'
+        self.result = self.vk.get(method,
+                                  owner_id=self.id)
+        self.response = self.result.get('response', None)
+        if self.response:
+            upload_url = self.response.get('upload_url', None)
+            file_up = {'file': (os.path.basename(file), open(file, 'rb'))}
+            uploaded = requests.post(upload_url, files=file_up)
+            if uploaded.ok:
+                method = 'docs.save'
+                self.result = self.vk.get(method,
+                                          owner_id=self.id,
+                                          file=uploaded.json()['file'])
+                self.response = self.result.get('response', None)
+                if self.response:
+                    owner_id = self.response[0]['owner_id']
+                    _id = self.response[0]['id']
+                    return 'doc' + str(owner_id) + str(_id)
